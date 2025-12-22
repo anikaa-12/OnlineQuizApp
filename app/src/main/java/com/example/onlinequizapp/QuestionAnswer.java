929807ae01a2ce18fca82aa52fc85c9d9b8b6b06
204-anika <2023200000204@seu.edu.bd>
2025-12-22 21:30:58 +0600

    Initial commit

---
+++ app/src/main/java/com/example/onlinequizapp/QuestionAnswer.java
@@ -0,0 +1,25 @@
+package com.example.onlinequizapp;
+
+public class QuestionAnswer {
+    public static String question[] ={
+            "Which company owns the android?",
+            "Which one is not the programming language?",
+            "Which SDLC model follows a step-by-step approach?",
+            "UML is mainly used for:"
+    };
+
+    public static String choices[][] ={
+            {"Google","Apple","Nokia","Samsung"},
+            {"Java","Kotlin","Notepad","Python"},
+            {"Agile","Spiral","Waterfall","RAD model"},
+            {"Writing code","Testing software","Database backup","System modeling"}
+    };
+
+    public static String correctAnswers[] ={
+            "Google",
+            "Notepad",
+            "Waterfall",
+            "System modeling"
+    };
+
+}
