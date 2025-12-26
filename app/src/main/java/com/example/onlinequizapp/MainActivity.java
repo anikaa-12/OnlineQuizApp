9d474f70e2325970ea04f212ac43bb8ee7dddb3f
204-anika <2023200000204@seu.edu.bd>
2025-12-26 23:15:08 +0600

    Edited MainActivity

--- app/src/main/java/com/example/onlinequizapp/MainActivity.java
+++ app/src/main/java/com/example/onlinequizapp/MainActivity.java
@@ -1,31 +1,6 @@
 package com.example.onlinequizapp;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-    }
-}
-package com.example.onlinequizapp;
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.AlertDialog;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -33,30 +8,29 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     TextView totalQuestionsTextView;
     TextView questionTextView;
     Button ansA, ansB, ansC, ansD;
     Button submitBtn;
 
-    int score = 0;
-    int totalQuestion = QuestionAnswer.question.length;
-    int currentQuestionIndex = 0;
-    String selectedAnswer = "";
+    int score=0;
+    int totalQuestion=QuestionAnswer.question.length;
+    int currentQuestionIndex=0;
+    String selectedAnswer="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        totalQuestionsTextView = findViewById(R.id.total_question);
-        questionTextView = findViewById(R.id.question);
-        ansA = findViewById(R.id.ans_A);
-        ansB = findViewById(R.id.ans_B);
-        ansC = findViewById(R.id.ans_C);
-        ansD = findViewById(R.id.ans_D);
-        submitBtn = findViewById(R.id.submit_btn);
+        totalQuestionsTextView=findViewById(R.id.total_question);
+        questionTextView=findViewById(R.id.question);
+        ansA=findViewById(R.id.ans_A);
+        ansB=findViewById(R.id.ans_B);
+        ansC=findViewById(R.id.ans_C);
+        ansD=findViewById(R.id.ans_D);
+        submitBtn=findViewById(R.id.submit_btn);
 
         ansA.setOnClickListener(this);
         ansB.setOnClickListener(this);
@@ -64,15 +38,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         ansD.setOnClickListener(this);
         submitBtn.setOnClickListener(this);
 
-        totalQuestionsTextView.setText("Total Questions : " + totalQuestion);
-
+        totalQuestionsTextView.setText("Total Questions : " +totalQuestion);
         loadNewQuestion();
     }
-
     @Override
-    public void onClick(View view) {
-
-        // Reset colors
+    public void onClick(View view){
         ansA.setBackgroundColor(Color.WHITE);
         ansB.setBackgroundColor(Color.WHITE);
         ansC.setBackgroundColor(Color.WHITE);
@@ -80,58 +50,45 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         Button clickedButton = (Button) view;
 
-        if (clickedButton.getId() == R.id.submit_btn) {
-
-            if (selectedAnswer.equals(QuestionAnswer.correctAnswers[currentQuestionIndex])) {
+        if (clickedButton.getId() == R.id.submit_btn){
+            if (selectedAnswer.equals(QuestionAnswer.correctAnswers[currentQuestionIndex])){
                 score++;
             }
-
             currentQuestionIndex++;
             loadNewQuestion();
+        }
+        else{
+            selectedAnswer=clickedButton.getText().toString();
 
-        } else {
-            // Option button clicked
-            selectedAnswer = clickedButton.getText().toString();
             clickedButton.setBackgroundColor(getResources().getColor(R.color.pink));
         }
     }
-
-    void loadNewQuestion() {
-
-        if (currentQuestionIndex == totalQuestion) {
+    void loadNewQuestion(){
+        if (currentQuestionIndex==totalQuestion){
             finishQuiz();
             return;
         }
-
         questionTextView.setText(QuestionAnswer.question[currentQuestionIndex]);
         ansA.setText(QuestionAnswer.choices[currentQuestionIndex][0]);
         ansB.setText(QuestionAnswer.choices[currentQuestionIndex][1]);
         ansC.setText(QuestionAnswer.choices[currentQuestionIndex][2]);
         ansD.setText(QuestionAnswer.choices[currentQuestionIndex][3]);
 
-        selectedAnswer = "";
+        selectedAnswer="";
     }
-
-    void finishQuiz() {
-
+    void finishQuiz(){
         String passStatus;
-        if (score >= totalQuestion * 0.6) {
-            passStatus = "Passed";
-        } else {
-            passStatus = "Failed";
+        if (score>=totalQuestion*0.6){
+            passStatus="Passed";
         }
-
-        new AlertDialog.Builder(this)
-                .setTitle(passStatus)
-                .setMessage("Score: " + score + " out of " + totalQuestion)
-                .setPositiveButton("Restart", (dialogInterface, i) -> restartQuiz())
-                .setCancelable(false)
-                .show();
+        else{
+            passStatus="Failed";
+        }
+        new AlertDialog.Builder(this).setTitle(passStatus).setMessage("Score: " +score+ "out of" +totalQuestion).setPositiveButton("Restart",(dialogInterface, i) -> restartQuiz()).setCancelable(false).show();
     }
-
-    void restartQuiz() {
-        score = 0;
-        currentQuestionIndex = 0;
+    void restartQuiz(){
+        score=0;
+        currentQuestionIndex=0;
         loadNewQuestion();
     }
 }
